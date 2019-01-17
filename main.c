a783b482f509ef51ee86151abf92b828db29c7a5
杨成光 <1213215489@qq.com>
2019-01-17 19:44:52 +0800

    fisrt init

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("huanyingjinruzhuanchangguanlixitong!\n");
+    
+    while (1)
+    {
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangweizhi\n");
+        printf("4---fanhuiliwozuijincheliangweizhi\n");
+        printf("5---tuichu\n");
+         
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d", &code);
+              if (code == 1)
+        {
+
+        }
+        if (code == 2)
+        {
+        }
+        if (code == 3)
+        {
+        }
+        if (code == 4)
+        {
+        }
+        if (code == 5)
+        {
+             printf("yingyongyijingtuichu\n");
+            break;
+        }
+    }
+        return 0;
+
+}
