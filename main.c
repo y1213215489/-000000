b3273c56915032a01993f8ebc0f69a408230a294
杨成光 <1213215489@qq.com>
2019-01-18 11:22:05 +0800

    tianjia

--- main.c
+++ main.c
@@ -74,12 +74,24 @@ int main(){
         scanf("%c",&x);
         scanf("%c",&x);
         }
-            
+        if(code==4)
+        {
+            for(int i = 0;i<index;i++){
+                int sum = 0;
+                sum=arr[i].yuwen+arr[i].shuxue+arr[i].yingyu;
+
+                if(sum<180){
+                    printf("D%dMxs%scjbjg\n",i+1,arr[i].name);
+                }
+            }
+            printf("Sxwc,djhcjx\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
         
     
-        if(code == 4){
-            
-        }
+       
         if(code == 5){
             
         }
